176749c0bad10a9b74f783eb2f8356177f779945
Ali Malluh <alimalluh@logestechs.com>
2023-11-02 09:56:00 +0200

    Initial Commit

---
+++ AliCard/AliCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AliCardApp.swift
+//  AliCard
+//
+//  Created by Ali-Logestechs on 02/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct AliCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
